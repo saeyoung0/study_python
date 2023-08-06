ea8515dbc8f313a94d798a5575f76b47122efdb2
saeyoung0 <72433558+saeyoung0@users.noreply.github.com>
2023-08-06 22:35:17 +0900

    Update 코드덤_3.py

--- 코드덤_3.py
+++ 코드덤_3.py
@@ -1,8 +1,1 @@
-Python 3.11.3 (tags/v3.11.3:f3909b8, Apr  4 2023, 23:49:59) [MSC v.1934 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
->>> 
-====================== RESTART: C:/Users/SAMSUNG/OneDrive/바탕 화면/ect/코드덤_3.py =====================
-이름을 적어주세요.코딩
->>> 
-====================== RESTART: C:/Users/SAMSUNG/OneDrive/바탕 화면/ect/코드덤_3.py =====================
-이름을 적어주세요.
+
